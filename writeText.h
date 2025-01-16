290ee40c780292c957cba443dd9a3ff766246d76
Alexander Belous <alexander.belous@outlook.com>
2025-01-16 16:32:26 +0100

    Delete H3MFwd.h.

--- writeText.h
+++ writeText.h
@@ -1,11 +1,11 @@
 #pragma once
 
-#include <h3mtxt/H3MFwd.h>
-
 #include <iosfwd>
 
 namespace h3m
 {
+  struct Map;
+
   // Writes a textual representation of the given H3M map into the given stream.
   // \param stream - output stream.
   // \param map - input H3M map.
