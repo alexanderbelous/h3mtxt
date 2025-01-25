d0572754bfdae731016afa4b179a3a290cdf5751
Alexander Belous <alexander.belous@outlook.com>
2025-01-25 05:20:31 +0100

    Add comment.

--- Map/SecondarySkillsBitmask.h
+++ Map/SecondarySkillsBitmask.h
@@ -7,6 +7,10 @@ namespace h3m
 {
   // Wrapper for BitSet<4> (which is itself a wrapper for an array of 4 bytes) that
   // stores a bit for each secondary skill.
+  //
+  // Note that there are only 28 secondary skills in Shadow of Death, but this bitmask
+  // stores 32 bits. The last 4 bits are normally set to 0 in .h3m regardless of the meaning
+  // of the bitmask.
   struct SecondarySkillsBitmask
   {
     constexpr bool operator[](SecondarySkillType secondary_skill) const
