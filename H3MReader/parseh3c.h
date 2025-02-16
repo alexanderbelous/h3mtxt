6d31f33b27f93979bd57f23424baab08eef55d68
Alexander Belous <alexander.belous@outlook.com>
2025-02-16 15:03:06 +0100

    Fix typo.

--- H3MReader/parseh3c.h
+++ H3MReader/parseh3c.h
@@ -9,6 +9,6 @@ namespace h3m
   // Reads a *.h3c campaign from the given stream.
   // \param stream - input stream. The stream is expected to store
   //        either gzip-compressed or uncompressed *.h3c data.
-  // \return the parsed map.
+  // \return the parsed campaign.
   Campaign parseh3c(std::istream& stream);
 }
