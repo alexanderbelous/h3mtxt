0c31b33dd76879abea2efd699fcfd8b5b2ee8e1f
Alexander Belous <alexander.belous@outlook.com>
2025-02-19 18:54:50 +0100

    Detect .h3c files when reading the input.

--- h3mtxt.cpp
+++ h3mtxt.cpp
@@ -1,8 +1,11 @@
+#include <h3mtxt/Campaign/Constants/CampaignFormat.h>
 #include <h3mtxt/H3MJsonReader/readH3mJson.h>
 #include <h3mtxt/H3MJsonWriter/writeH3mJson.h>
 #include <h3mtxt/H3MReader/parseh3m.h>
 #include <h3mtxt/H3MWriter/writeh3m.h>
 #include <h3mtxt/Map/Constants/MapFormat.h>
+
+#include <h3mtxt/thirdparty/zstr/src/zstr.hpp>
 
 #include <filesystem>
 #include <fstream>
@@ -13,10 +16,18 @@ namespace fs = std::filesystem;
 
 namespace
 {
-  // \return true if the specified stream contains a H3M file,
-  //         false if it contains a JSON file.
-  //         The function doesn't check if the stream contains a valid file.
-  bool isH3mFile(std::istream& stream)
+  enum class FileType
+  {
+    GZip,
+    UncompressedH3M,
+    UncompressedH3C,
+    Other
+  };
+
+  // Determines the type of the file by reading the 0th byte without extracting it.
+  // \param stream - input stream containing the file data.
+  // \return FileType for the file contained in @stream.
+  FileType getFileType(std::istream& stream)
   {
     constexpr char kGzipFirstByte = 0x1F;
 
@@ -24,12 +35,70 @@ namespace
     switch (first_byte)
     {
     case kGzipFirstByte:
-    case static_cast<char>(h3m::MapFormat::RestorationOfErathia):
-    case static_cast<char>(h3m::MapFormat::ArmageddonsBlade):
-    case static_cast<char>(h3m::MapFormat::ShadowOfDeath):
-      return true;
+      return FileType::GZip;
+    case static_cast<std::uint8_t>(h3m::MapFormat::RestorationOfErathia):
+    case static_cast<std::uint8_t>(h3m::MapFormat::ArmageddonsBlade):
+    case static_cast<std::uint8_t>(h3m::MapFormat::ShadowOfDeath):
+      return FileType::UncompressedH3M;
+    case static_cast<std::uint8_t>(h3m::CampaignFormat::RestorationOfErathia):
+    case static_cast<std::uint8_t>(h3m::CampaignFormat::ArmageddonsBlade):
+    case static_cast<std::uint8_t>(h3m::CampaignFormat::ShadowOfDeath):
+      return FileType::UncompressedH3C;
     default:
-      return false;
+      return FileType::Other;
+    }
+  }
+
+  struct Input
+  {
+    // TODO: replace with std::variant<h3m::Map, h3m::Campaign>.
+    h3m::Map map;
+    // True if the input file is a JSON document, false otherwise.
+    bool is_json {};
+  };
+
+  // Reads a h3m::Map or a h3m::Campaign from the input stream.
+  // TODO: change the type of the returned value to std::variant<h3m::Map, h3m::Campaign>.
+  h3m::Map readUncompressedH3(std::istream& stream)
+  {
+    switch (getFileType(stream))
+    {
+    case FileType::UncompressedH3M:
+      return h3m::parseh3m(stream);
+    case FileType::UncompressedH3C:
+      throw std::runtime_error(".h3c files are not supported yet.");
+    default:
+      throw std::runtime_error("Unknown file format.");
+    }
+  }
+
+  Input readInput(std::istream& stream)
+  {
+    const FileType file_type = getFileType(stream);
+    switch (file_type)
+    {
+    case FileType::GZip:
+    {
+      zstr::istream zstr_stream(stream);
+      return Input{ .map = readUncompressedH3(zstr_stream), .is_json = false };
+    }
+    case FileType::UncompressedH3C:
+    case FileType::UncompressedH3M:
+      return Input{ .map = readUncompressedH3(stream), .is_json = false };
+    default:
+      return Input{ .map = h3m::readH3mJson(stream), .is_json = true };
+    }
+  }
+
+  void writeOutput(std::ostream& stream, const Input& input)
+  {
+    if (input.is_json)
+    {
+      h3m::writeh3m(stream, input.map);
+    }
+    else
+    {
+      h3m::writeH3mJson(stream, input.map);
     }
   }
 }
@@ -60,8 +129,7 @@ int main(int argc, char** argv)
       std::cerr << "Failed to open: " << path_input.string();
       return -1;
     }
-    const bool is_h3m_file = isH3mFile(stream);
-    const h3m::Map map = is_h3m_file ? h3m::parseh3m(stream) : h3m::readH3mJson(stream);
+    const Input input = readInput(stream);
     stream.close();
     std::ofstream out_stream(path_output, std::ios_base::out | std::ios_base::binary);
     if (!out_stream)
@@ -69,14 +137,7 @@ int main(int argc, char** argv)
       std::cerr << "Failed to open: " << path_output.string();
       return -1;
     }
-    if (is_h3m_file)
-    {
-      h3m::writeH3mJson(out_stream, map);
-    }
-    else
-    {
-      h3m::writeh3m(out_stream, map);
-    }
+    writeOutput(out_stream, input);
     out_stream.close();
   }
   catch (const std::exception& error)
