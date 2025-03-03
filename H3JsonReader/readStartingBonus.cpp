0d274b2bedcc491726772b0eea13e8bb1bb127d3
Alexander Belous <alexander.belous@outlook.com>
2025-03-03 23:51:31 +0100

    Specialize JsonReader for all StartingBonusDetails.

--- H3JsonReader/readStartingBonus.cpp
+++ H3JsonReader/readStartingBonus.cpp
@@ -3,19 +3,8 @@
 #include <h3mtxt/JsonCommon/FieldName.h>
 #include <h3mtxt/Campaign/StartingBonus.h>
 
-#include <stdexcept>
-
 namespace h3m::H3JsonReader_NS
 {
-  template<StartingBonusType T>
-  struct JsonReader<StartingBonusDetails<T>>
-  {
-    StartingBonusDetails<T> operator()(const Json::Value&) const
-    {
-      throw std::runtime_error("JsonReader<StartingBonusDetails<T>>: NotImplemented.");
-    }
-  };
-
   template<>
   struct JsonReader<StartingBonusDetails<StartingBonusType::Spell>>
   {
@@ -36,6 +25,77 @@ namespace h3m::H3JsonReader_NS
       StartingBonusDetails<StartingBonusType::Creature> details;
       readField(details.hero, value, "hero");
       readField(details.creature_stack, value, "creature_stack");
+      return details;
+    }
+  };
+
+  template<>
+  struct JsonReader<StartingBonusDetails<StartingBonusType::Building>>
+  {
+    StartingBonusDetails<StartingBonusType::Building> operator()(const Json::Value& value) const
+    {
+      StartingBonusDetails<StartingBonusType::Building> details;
+      readField(details.building, value, "building");
+      return details;
+    }
+  };
+
+  template<>
+  struct JsonReader<StartingBonusDetails<StartingBonusType::Artifact>>
+  {
+    StartingBonusDetails<StartingBonusType::Artifact> operator()(const Json::Value& value) const
+    {
+      StartingBonusDetails<StartingBonusType::Artifact> details;
+      readField(details.hero, value, "hero");
+      readField(details.artifact, value, "artifact");
+      return details;
+    }
+  };
+
+  template<>
+  struct JsonReader<StartingBonusDetails<StartingBonusType::SpellScroll>>
+  {
+    StartingBonusDetails<StartingBonusType::SpellScroll> operator()(const Json::Value& value) const
+    {
+      StartingBonusDetails<StartingBonusType::SpellScroll> details;
+      readField(details.hero, value, "hero");
+      readField(details.spell, value, "spell");
+      return details;
+    }
+  };
+
+  template<>
+  struct JsonReader<StartingBonusDetails<StartingBonusType::PrimarySkills>>
+  {
+    StartingBonusDetails<StartingBonusType::PrimarySkills> operator()(const Json::Value& value) const
+    {
+      StartingBonusDetails<StartingBonusType::PrimarySkills> details;
+      readField(details.hero, value, "hero");
+      readField(details.skills, value, "skills");
+      return details;
+    }
+  };
+
+  template<>
+  struct JsonReader<StartingBonusDetails<StartingBonusType::SecondarySkill>>
+  {
+    StartingBonusDetails<StartingBonusType::SecondarySkill> operator()(const Json::Value& value) const
+    {
+      StartingBonusDetails<StartingBonusType::SecondarySkill> details;
+      readField(details.hero, value, "hero");
+      readField(details.skill, value, "skill");
+      return details;
+    }
+  };
+
+  template<>
+  struct JsonReader<StartingBonusDetails<StartingBonusType::Resource>>
+  {
+    StartingBonusDetails<StartingBonusType::Resource> operator()(const Json::Value& value) const
+    {
+      StartingBonusDetails<StartingBonusType::Resource> details;
+      readField(details.type, value, "type");
+      readField(details.amount, value, "amount");
       return details;
     }
   };
