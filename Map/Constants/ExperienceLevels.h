fd189ced39458daa07572297eb2eaf4c8d52a205
Alexander Belous <alexander.belous@outlook.com>
2025-03-08 19:07:14 +0100

    Code refactoring.

--- Map/Constants/ExperienceLevels.h
+++ Map/Constants/ExperienceLevels.h
@@ -86,108 +86,105 @@ namespace h3m
     {
       return Detail_NS::kExperienceForLevel[level];
     }
-    // Experience needed to achieve level 12.
+    // f(i) = experience needed to achieve level i; i == 12 initially.
     std::int32_t experience = Detail_NS::kExperienceForLevel[12];
-    // Difference (in experience points) between level 12 and level 11.
-    std::int32_t exp_diff_prev = Detail_NS::kExperienceForLevel[12] - Detail_NS::kExperienceForLevel[11];
+    // diff(i) = f(i) - f(i-1); i == 12 initially.
+    std::int32_t exp_diff = Detail_NS::kExperienceForLevel[12] - Detail_NS::kExperienceForLevel[11];
     // For "abnormal" levels: keep track of the highest encountered experience threshold.
     std::int32_t highest_experience = 0;
     // TODO: while it's cool that this formula covers all supported levels, it would be inefficient to
     // do 6412 iterations to compute getExperienceForLevel(6246). As an optimization, consider handling all
     // abnormal levels outside the loop.
-    for (std::uint16_t i = 12; i < level; ++i)
+    for (std::uint32_t i = 12; i < level; ++i)
     {
-      const std::uint32_t experience_prev_unsigned = static_cast<std::uint32_t>(experience);
-      // Calculate the difference between f(i) and f(i+1).
-      // This is basically exp_diff_prev * 1.2, but overflow must be handled carefully.
-      const std::uint32_t exp_diff_unsigned =
-        static_cast<std::uint32_t>(exp_diff_prev) + static_cast<std::uint32_t>(exp_diff_prev / 5);
-      // Update experience to store f(i+1).
-      experience = static_cast<std::int32_t>(experience_prev_unsigned + exp_diff_unsigned);
-      // Update highest_experience if needed.
+      // Update `exp_diff` to store diff(i+1) = f(i+1) - f(i).
+      // This is basically multiplying by 1.2, but overflow must be handled carefully.
+      exp_diff = static_cast<std::int32_t>(
+        static_cast<std::uint32_t>(exp_diff) + static_cast<std::uint32_t>(exp_diff / 5));
+      // Update `experience` to store f(i+1).
+      experience = static_cast<std::int32_t>(
+        static_cast<std::uint32_t>(experience) + static_cast<std::uint32_t>(exp_diff));
+      // Update `highest_experience` if needed.
       if (experience > highest_experience)
       {
         highest_experience = experience;
       }
-      // Update exp_diff_prev to store f(i+1) - f(i).
-      // This is equivalent to static_cast<std::int32_t>(experience_unsigned - experience_prev_unsigned).
-      exp_diff_prev = static_cast<std::int32_t>(exp_diff_unsigned);
     }
     // return `experience` instead if you want to get the cap for MapBasicInfo::max_hero_level.
     return highest_experience;
   }
 
-  static_assert(getExperienceForLevel(13) ==        24'320);
-  static_assert(getExperienceForLevel(14) ==        28'784);
-  static_assert(getExperienceForLevel(15) ==        34'140);
-  static_assert(getExperienceForLevel(16) ==        40'567);
-  static_assert(getExperienceForLevel(17) ==        48'279);
-  static_assert(getExperienceForLevel(18) ==        57'533);
-  static_assert(getExperienceForLevel(19) ==        68'637);
-  static_assert(getExperienceForLevel(20) ==        81'961);
-  static_assert(getExperienceForLevel(21) ==        97'949);
-  static_assert(getExperienceForLevel(22) ==       117'134);
-  static_assert(getExperienceForLevel(23) ==       140'156);
-  static_assert(getExperienceForLevel(24) ==       167'782);
-  static_assert(getExperienceForLevel(25) ==       200'933);
-  static_assert(getExperienceForLevel(26) ==       240'714);
-  static_assert(getExperienceForLevel(27) ==       288'451);
-  static_assert(getExperienceForLevel(28) ==       345'735);
-  static_assert(getExperienceForLevel(29) ==       414'475);
-  static_assert(getExperienceForLevel(30) ==       496'963);
-  static_assert(getExperienceForLevel(31) ==       595'948);
-  static_assert(getExperienceForLevel(32) ==       714'730);
-  static_assert(getExperienceForLevel(33) ==       857'268);
-  static_assert(getExperienceForLevel(34) ==     1'028'313);
-  static_assert(getExperienceForLevel(35) ==     1'233'567);
-  static_assert(getExperienceForLevel(36) ==     1'479'871);
-  static_assert(getExperienceForLevel(37) ==     1'775'435);
-  static_assert(getExperienceForLevel(38) ==     2'130'111);
-  static_assert(getExperienceForLevel(39) ==     2'555'722);
-  static_assert(getExperienceForLevel(40) ==     3'066'455);
-  static_assert(getExperienceForLevel(41) ==     3'679'334);
-  static_assert(getExperienceForLevel(42) ==     4'414'788);
-  static_assert(getExperienceForLevel(43) ==     5'297'332);
-  static_assert(getExperienceForLevel(44) ==     6'356'384);
-  static_assert(getExperienceForLevel(45) ==     7'627'246);
-  static_assert(getExperienceForLevel(46) ==     9'152'280);
-  static_assert(getExperienceForLevel(47) ==    10'982'320);
-  static_assert(getExperienceForLevel(48) ==    13'178'368);
-  static_assert(getExperienceForLevel(49) ==    15'813'625);
-  static_assert(getExperienceForLevel(50) ==    18'975'933);
-  static_assert(getExperienceForLevel(51) ==    22'770'702);
-  static_assert(getExperienceForLevel(52) ==    27'324'424);
-  static_assert(getExperienceForLevel(53) ==    32'788'890);
-  static_assert(getExperienceForLevel(54) ==    39'346'249);
-  static_assert(getExperienceForLevel(55) ==    47'215'079);
-  static_assert(getExperienceForLevel(56) ==    56'657'675);
-  static_assert(getExperienceForLevel(57) ==    67'988'790);
-  static_assert(getExperienceForLevel(58) ==    81'586'128);
-  static_assert(getExperienceForLevel(59) ==    97'902'933);
-  static_assert(getExperienceForLevel(60) ==   117'483'099);
-  static_assert(getExperienceForLevel(61) ==   140'979'298);
-  static_assert(getExperienceForLevel(62) ==   169'174'736);
-  static_assert(getExperienceForLevel(63) ==   203'009'261);
-  static_assert(getExperienceForLevel(64) ==   243'610'691);
-  static_assert(getExperienceForLevel(65) ==   292'332'407);
-  static_assert(getExperienceForLevel(66) ==   350'798'466);
-  static_assert(getExperienceForLevel(67) ==   420'957'736);
-  static_assert(getExperienceForLevel(68) ==   505'148'860);
-  static_assert(getExperienceForLevel(69) ==   606'178'208);
-  static_assert(getExperienceForLevel(70) ==   727'413'425);
-  static_assert(getExperienceForLevel(71) ==   872'895'685);
-  static_assert(getExperienceForLevel(72) == 1'047'474'397);
-  static_assert(getExperienceForLevel(73) == 1'256'968'851);
-  static_assert(getExperienceForLevel(74) == 1'508'362'195);
+  static_assert(getExperienceForLevel(13)   ==         24'320);
+  static_assert(getExperienceForLevel(14)   ==         28'784);
+  static_assert(getExperienceForLevel(15)   ==         34'140);
+  static_assert(getExperienceForLevel(16)   ==         40'567);
+  static_assert(getExperienceForLevel(17)   ==         48'279);
+  static_assert(getExperienceForLevel(18)   ==         57'533);
+  static_assert(getExperienceForLevel(19)   ==         68'637);
+  static_assert(getExperienceForLevel(20)   ==         81'961);
+  static_assert(getExperienceForLevel(21)   ==         97'949);
+  static_assert(getExperienceForLevel(22)   ==        117'134);
+  static_assert(getExperienceForLevel(23)   ==        140'156);
+  static_assert(getExperienceForLevel(24)   ==        167'782);
+  static_assert(getExperienceForLevel(25)   ==        200'933);
+  static_assert(getExperienceForLevel(26)   ==        240'714);
+  static_assert(getExperienceForLevel(27)   ==        288'451);
+  static_assert(getExperienceForLevel(28)   ==        345'735);
+  static_assert(getExperienceForLevel(29)   ==        414'475);
+  static_assert(getExperienceForLevel(30)   ==        496'963);
+  static_assert(getExperienceForLevel(31)   ==        595'948);
+  static_assert(getExperienceForLevel(32)   ==        714'730);
+  static_assert(getExperienceForLevel(33)   ==        857'268);
+  static_assert(getExperienceForLevel(34)   ==      1'028'313);
+  static_assert(getExperienceForLevel(35)   ==      1'233'567);
+  static_assert(getExperienceForLevel(36)   ==      1'479'871);
+  static_assert(getExperienceForLevel(37)   ==      1'775'435);
+  static_assert(getExperienceForLevel(38)   ==      2'130'111);
+  static_assert(getExperienceForLevel(39)   ==      2'555'722);
+  static_assert(getExperienceForLevel(40)   ==      3'066'455);
+  static_assert(getExperienceForLevel(41)   ==      3'679'334);
+  static_assert(getExperienceForLevel(42)   ==      4'414'788);
+  static_assert(getExperienceForLevel(43)   ==      5'297'332);
+  static_assert(getExperienceForLevel(44)   ==      6'356'384);
+  static_assert(getExperienceForLevel(45)   ==      7'627'246);
+  static_assert(getExperienceForLevel(46)   ==      9'152'280);
+  static_assert(getExperienceForLevel(47)   ==     10'982'320);
+  static_assert(getExperienceForLevel(48)   ==     13'178'368);
+  static_assert(getExperienceForLevel(49)   ==     15'813'625);
+  static_assert(getExperienceForLevel(50)   ==     18'975'933);
+  static_assert(getExperienceForLevel(51)   ==     22'770'702);
+  static_assert(getExperienceForLevel(52)   ==     27'324'424);
+  static_assert(getExperienceForLevel(53)   ==     32'788'890);
+  static_assert(getExperienceForLevel(54)   ==     39'346'249);
+  static_assert(getExperienceForLevel(55)   ==     47'215'079);
+  static_assert(getExperienceForLevel(56)   ==     56'657'675);
+  static_assert(getExperienceForLevel(57)   ==     67'988'790);
+  static_assert(getExperienceForLevel(58)   ==     81'586'128);
+  static_assert(getExperienceForLevel(59)   ==     97'902'933);
+  static_assert(getExperienceForLevel(60)   ==    117'483'099);
+  static_assert(getExperienceForLevel(61)   ==    140'979'298);
+  static_assert(getExperienceForLevel(62)   ==    169'174'736);
+  static_assert(getExperienceForLevel(63)   ==    203'009'261);
+  static_assert(getExperienceForLevel(64)   ==    243'610'691);
+  static_assert(getExperienceForLevel(65)   ==    292'332'407);
+  static_assert(getExperienceForLevel(66)   ==    350'798'466);
+  static_assert(getExperienceForLevel(67)   ==    420'957'736);
+  static_assert(getExperienceForLevel(68)   ==    505'148'860);
+  static_assert(getExperienceForLevel(69)   ==    606'178'208);
+  static_assert(getExperienceForLevel(70)   ==    727'413'425);
+  static_assert(getExperienceForLevel(71)   ==    872'895'685);
+  static_assert(getExperienceForLevel(72)   ==  1'047'474'397);
+  static_assert(getExperienceForLevel(73)   ==  1'256'968'851);
+  static_assert(getExperienceForLevel(74)   ==  1'508'362'195);
   // Nonstandard levels.
-  static_assert(getExperienceForLevel(75) ==   1'810'034'207);
-  static_assert(getExperienceForLevel(88) ==   1'810'034'207);
-  static_assert(getExperienceForLevel(100) ==  2'073'739'175);
-  static_assert(getExperienceForLevel(108) ==  2'099'639'276);
-  static_assert(getExperienceForLevel(868) ==  2'144'641'867);
-  static_assert(getExperienceForLevel(3732) == 2'146'553'679);
-  static_assert(getExperienceForLevel(5920) == 2'146'673'313);
-  static_assert(getExperienceForLevel(6424) == 2'147'293'156);
+  static_assert(getExperienceForLevel(75)   ==  1'810'034'207);
+  static_assert(getExperienceForLevel(88)   ==  1'810'034'207);
+  static_assert(getExperienceForLevel(100)  ==  2'073'739'175);
+  static_assert(getExperienceForLevel(108)  ==  2'099'639'276);
+  static_assert(getExperienceForLevel(868)  ==  2'144'641'867);
+  static_assert(getExperienceForLevel(3732) ==  2'146'553'679);
+  static_assert(getExperienceForLevel(5920) ==  2'146'673'313);
+  static_assert(getExperienceForLevel(6424) ==  2'147'293'156);
 
   // Disregard, this is out of date.
   // These values are not for levels but for the experience thresholds yielded by MapBasicInfo::max_hero_level.
