41e5b42ce5ab177a360b68ca608599f32705c00d
Alexander Belous <alexander.belous@outlook.com>
2025-03-19 09:44:45 +0100

    Add static_asserts in writeValue().

--- JsonWriter/writeValue.h
+++ JsonWriter/writeValue.h
@@ -24,30 +24,28 @@ namespace Medea_NS
 
       if constexpr (Traits::kValueType == JsonValueType::Bool)
       {
-        using GetValueResultType = decltype(Traits::getValue(value));
-        static_assert(std::is_same_v<GetValueResultType, bool>,
+        static_assert(std::is_same_v<decltype(Traits::getValue(value)), bool>,
                       "JsonWriterTraits<T>::getValue(value) must return bool.");
         context.writeBool(Traits::getValue(value));
       }
       else if constexpr (Traits::kValueType == JsonValueType::Int)
       {
-        using GetValueResultType = decltype(Traits::getValue(value));
-        //static_assert(std::is_integeral_v<GetValueResultType> && std::is_signed_v<GetValueResultType>,
-        //              "JsonWriterTraits<T>::getValue(value) must return a signed integer.");
+        static_assert(std::is_integral_v<decltype(Traits::getValue(value))> &&
+                      std::is_signed_v<decltype(Traits::getValue(value))>,
+                      "JsonWriterTraits<T>::getValue(value) must return a signed integer.");
         context.writeInt(static_cast<std::intmax_t>(Traits::getValue(value)));
       }
       else if constexpr (Traits::kValueType == JsonValueType::UInt)
       {
-        using GetValueResultType = decltype(Traits::getValue(value));
-        //static_assert(std::is_integeral_v<GetValueResultType> && std::is_unsigned_v<GetValueResultType>,
-        //              "JsonWriterTraits<T>::getValue(value) must return an unsigned integer.");
+        static_assert(std::is_integral_v<decltype(Traits::getValue(value))> &&
+                      std::is_unsigned_v<decltype(Traits::getValue(value))>,
+                      "JsonWriterTraits<T>::getValue(value) must return an unsigned integer.");
         context.writeUInt(static_cast<std::uintmax_t>(Traits::getValue(value)));
       }
       else if constexpr (Traits::kValueType == JsonValueType::String)
       {
-        using GetValueResultType = decltype(Traits::getValue(value));
-        //static_assert(std::is_convertible_v<GetValueResultType, std::string_view>,
-        //              "JsonWriterTraits<T>::getValue(value) must be convertible to std::string_view.");
+        static_assert(std::is_convertible_v<decltype(Traits::getValue(value)), std::string_view>,
+                      "JsonWriterTraits<T>::getValue(value) must be convertible to std::string_view.");
         context.writeString(Traits::getValue(value));
       }
       else if constexpr (Traits::kValueType == JsonValueType::Array)
