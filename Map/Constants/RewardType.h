1dcf61fb6581735d8273de1e6821179bc36163d9
Alexander Belous <alexander.belous@outlook.com>
2025-12-16 16:40:33 +0100

    Add a comment regarding values of RewardType outside [0; 10].
    Short version: no "hidden" rewards, don't use such values.

--- Map/Constants/RewardType.h
+++ Map/Constants/RewardType.h
@@ -17,6 +17,11 @@ namespace h3m
     Artifact = 8,
     Spell = 9,
     Creature = 10,
+    // FYI: there seem to be no "hidden" types here. If you try using values outside [0; 10],
+    // the game will treat them the same way as None, but the Map Editor will crash while
+    // trying to open the map.
+    // Note that h3mtxt itself does not support values outside [0; 10]: h3m::Reward cannot represent
+    // such rewards, so attempting to read or write maps with such rewards will lead to an error.
   };
 
   inline constexpr std::uint8_t kNumRewardTypes = 11;
