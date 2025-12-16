dbd124d34f66f990c58f76be84d7f30f0e4f4a29
Alexander Belous <alexander.belous@outlook.com>
2025-12-16 16:50:32 +0100

    Update comment.

--- Map/Constants/QuestType.h
+++ Map/Constants/QuestType.h
@@ -17,8 +17,8 @@ namespace h3m
     BeHero = 8,
     BePlayer = 9
     // FYI: there seem to be no "hidden" types here. If you try using values outside [0; 9],
-    // the game will treat them the same way as None, but the Map Editor will report
-    // "Invalid or corrupt map file".
+    // the game will treat them the same way as None, but the Map Editor will crash while
+    // trying to open the map.
     // Note that h3mtxt itself does not support values outside [0; 9]: h3m::Quest cannot represent
     // such quests, so attempting to read or write maps with such quests will lead to an error.
   };
