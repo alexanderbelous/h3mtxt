508e021f41ce2ec5352252e2dac1c64e94ea19d7
Alexander Belous <alexander.belous@outlook.com>
2026-02-26 14:38:39 +0100

    Remove the duplicate definition of ObjectTemplateSvg.

--- SavedGame/ObjectTemplateSvg.h
+++ SavedGame/ObjectTemplateSvg.h
@@ -20,9 +20,14 @@ namespace h3m
     // Height of the sprite (in tiles).
     std::uint8_t height {};
     // TODO: identify.
+    // possibly - a 6x8 boolean matrix, where A[i][j] indicates whether the sprite tile (i, j)
+    // exists, i.e. A[i][j] = (5 - i < height) && (7 - j < width).
+    // Actually, it's not necessarily redundant - it's possible that sprite tiles with no visible
+    // pixels are filtered out.
     std::array<std::byte, 6> unknown1 {};
     std::array<std::uint8_t, 6> passability {};
     // TODO: identify.
+    // possibly - similar to @unknown1, but for shadows.
     std::array<std::byte, 6> unknown2{};
     std::array<std::uint8_t, 6> actionability {};
     std::uint16_t object_class {};
--- SavedGame/SavedGame.h
+++ SavedGame/SavedGame.h
@@ -6,6 +6,7 @@
 #include <h3mtxt/Map/MapAdditionalInfo.h>
 #include <h3mtxt/Map/MapBasicInfo.h>
 #include <h3mtxt/Map/Utils/ReservedData.h>
+#include <h3mtxt/SavedGame/ObjectTemplateSvg.h>
 #include <h3mtxt/SavedGame/PlayerSpecsSvg.h>
 #include <h3mtxt/SavedGame/TileSvg.h>
 
@@ -26,32 +27,6 @@ namespace h3m
     // TODO: check if values other than 0 can be used in the game. If not, then this
     // field can be removed altogether.
     std::byte unknown {};
-  };
-
-  // The equivalent of h3m::ObjectTemplate stored in the saved game.
-  struct ObjectTemplateSvg
-  {
-    // Length-prefixed string; length is serialized as a 16-bit little-endian integer.
-    std::string def;
-    // Width of the sprite (in tiles).
-    std::uint8_t width {};
-    // Height of the sprite (in tiles).
-    std::uint8_t height {};
-    // TODO: identify.
-    // possibly - a 6x8 boolean matrix, where A[i][j] indicates whether the sprite tile (i, j)
-    // exists, i.e. A[i][j] = (5 - i < height) && (7 - j < width).
-    std::array<std::byte, 6> unknown1 {};
-    std::array<std::uint8_t, 6> passability {};
-    // TODO: identify.
-    // possibly - similar to @unknown1, but for shadows.
-    std::array<std::byte, 6> unknown2 {};
-    std::array<std::uint8_t, 6> actionability {};
-    std::uint16_t object_class {};
-    std::uint16_t object_subclass {};
-    // TODO: identify.
-    // Seems to be always zero, so it makes sense to replace it with ReservedData<2>
-    std::array<std::byte, 2> unknown3 {};
-    Bool is_ground {};
   };
 
   // Represents a saved game for Heroes of Might and Magic 3 (.GM1, .GM2, ... files).
@@ -169,7 +144,6 @@ namespace h3m
 
     // TODO: reverse-engineer the rest.
     // The next fields are approximately:
-    // * Object templates
     // * Objects on the Adventure map
     // * Settings for each hero
     //
