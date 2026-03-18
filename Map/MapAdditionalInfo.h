9e9cd8bdddf2fb47930099db1e85a58949cf4eba
Alexander Belous <alexander.belous@outlook.com>
2026-03-18 22:52:53 +0100

    Change the type of TeamsInfo::team_for_player to EnumIndexedArray.
    This has zero overhead in the code, but allows accessing the elements using
    PlayerColor enum value instead of integers.
    
    In JSON this improves readability: instead of
      "team_for_player": [0, 1, 2, 3, 1, 1, 4, 5]
    you now get
      "team_for_player": {
        "red": 0,
        "blue": 1,
        "tan": 2,
        "green": 3,
        "orange": 1,
        "purple": 1,
        "teal": 4,
        "pink": 5
      }

--- Map/MapAdditionalInfo.h
+++ Map/MapAdditionalInfo.h
@@ -4,7 +4,9 @@
 #include <h3mtxt/Map/Constants/Constants.h>
 #include <h3mtxt/Map/Constants/HeroPortrait.h>
 #include <h3mtxt/Map/Constants/HeroType.h>
+#include <h3mtxt/Map/Constants/PlayerColor.h>
 #include <h3mtxt/Map/Utils/EnumBitmask.h>
+#include <h3mtxt/Map/Utils/EnumIndexedArray.h>
 #include <h3mtxt/Map/Utils/ReservedData.h>
 #include <h3mtxt/Map/HeroSettings.h>
 #include <h3mtxt/Map/LossCondition.h>
@@ -20,113 +22,111 @@
 
 namespace h3m
 {
-
-struct TeamsInfo
-{
-  std::uint8_t num_teams {};
-  // This field is only read/written if num_teams != 0.
-  // Each value should be < num_teams.
-  std::array<std::uint8_t, 8> team_for_player {};
-};
-
-struct CustomHero
-{
-  HeroType type{};
-  // 0xFF for default.
-  HeroPortrait portrait = HeroPortrait::DEFAULT;
-  // Hero's name, or an empty string if the default name should be used instead.
-  std::string name;
-  // Bitmask indicating which players can hire this hero.
-  PlayersBitmask can_hire;
-};
-
-struct Rumor
-{
-  std::string name;
-  std::string description;
-};
-
-// Stores settings for all heroes.
-//
-// In .h3m these are stored like std::array<std::optional<HeroSettings>, kNumHeroes> -
-// a boolean value is written first, indicating if this hero has non-default settings;
-// HeroSettings is only written if this boolean value is true.
-//
-// However, generally only a few heroes have non-default settings in MapAdditionalInfo
-// (they are more likely to be customized for heroes on the adventure map), so it would
-// be memory-inefficient to explicitly store 156 values (whether as std::optional or as a pointer).
-class HeroesSettings
-{
-public:
-  HeroesSettings() = default;
-
-  inline bool hasSettings(HeroType hero) const;
-
-  inline HeroSettings& operator[](HeroType hero);
-
-  inline const HeroSettings& operator[](HeroType hero) const;
-
-  inline void erase(HeroType hero);
-
-  inline const std::map<HeroType, HeroSettings>& settings() const noexcept;
-
-private:
-  std::map<HeroType, HeroSettings> settings_;
-};
-
-bool HeroesSettings::hasSettings(HeroType hero) const
-{
-  return settings_.find(hero) != settings_.end();
-}
-
-HeroSettings& HeroesSettings::operator[](HeroType hero)
-{
-  if (static_cast<std::underlying_type_t<HeroType>>(hero) >= kNumHeroes)
+  struct TeamsInfo
   {
-    throw std::runtime_error("HeroesSettings::operator[]: invalid hero.");
+    std::uint8_t num_teams {};
+    // This field is only read/written if num_teams != 0.
+    // Each value should be < num_teams.
+    EnumIndexedArray<PlayerColor, std::uint8_t, kMaxPlayers> team_for_player;
+  };
+
+  struct CustomHero
+  {
+    HeroType type{};
+    // 0xFF for default.
+    HeroPortrait portrait = HeroPortrait::DEFAULT;
+    // Hero's name, or an empty string if the default name should be used instead.
+    std::string name;
+    // Bitmask indicating which players can hire this hero.
+    PlayersBitmask can_hire;
+  };
+
+  struct Rumor
+  {
+    std::string name;
+    std::string description;
+  };
+
+  // Stores settings for all heroes.
+  //
+  // In .h3m these are stored like std::array<std::optional<HeroSettings>, kNumHeroes> -
+  // a boolean value is written first, indicating if this hero has non-default settings;
+  // HeroSettings is only written if this boolean value is true.
+  //
+  // However, generally only a few heroes have non-default settings in MapAdditionalInfo
+  // (they are more likely to be customized for heroes on the adventure map), so it would
+  // be memory-inefficient to explicitly store 156 values (whether as std::optional or as a pointer).
+  class HeroesSettings
+  {
+  public:
+    HeroesSettings() = default;
+
+    inline bool hasSettings(HeroType hero) const;
+
+    inline HeroSettings& operator[](HeroType hero);
+
+    inline const HeroSettings& operator[](HeroType hero) const;
+
+    inline void erase(HeroType hero);
+
+    inline const std::map<HeroType, HeroSettings>& settings() const noexcept;
+
+  private:
+    std::map<HeroType, HeroSettings> settings_;
+  };
+
+  bool HeroesSettings::hasSettings(HeroType hero) const
+  {
+    return settings_.find(hero) != settings_.end();
   }
-  return settings_[hero];
-}
 
-const HeroSettings& HeroesSettings::operator[](HeroType hero) const
-{
-  return settings_.at(hero);
-}
+  HeroSettings& HeroesSettings::operator[](HeroType hero)
+  {
+    if (static_cast<std::underlying_type_t<HeroType>>(hero) >= kNumHeroes)
+    {
+      throw std::runtime_error("HeroesSettings::operator[]: invalid hero.");
+    }
+    return settings_[hero];
+  }
 
-void HeroesSettings::erase(HeroType hero)
-{
-  settings_.erase(hero);
-}
+  const HeroSettings& HeroesSettings::operator[](HeroType hero) const
+  {
+    return settings_.at(hero);
+  }
 
-const std::map<HeroType, HeroSettings>& HeroesSettings::settings() const noexcept
-{
-  return settings_;
-}
+  void HeroesSettings::erase(HeroType hero)
+  {
+    settings_.erase(hero);
+  }
 
-struct MapAdditionalInfo
-{
-  VictoryCondition victory_condition {};
-  LossCondition loss_condition {};
-  TeamsInfo teams;
-  // 1 bit per hero:
-  // * 0 if the hero is disabled (Map Specifications -> Heroes in the Map Editor) or already placed on the map.
-  // * 1 otherwise.
-  HeroesBitmask heroes_availability;
-  std::vector<HeroType> placeholder_heroes;
-  std::vector<CustomHero> custom_heroes;
-  // Reserved data; 0s by default.
-  ReservedData<31> reserved {};
-  // 1 bit per artifact; 1 means disabled, 0 means enabled.
-  // Spell Book, War Machines and Grail are not affected by this bitmask: they are enabled by default
-  // by the Map Editor, but you will be able to purchase them (or dig them out in case of Grail) even
-  // if you disable them manually.
-  ArtifactsBitmask disabled_artifacts;
-  // 1 bit per spell; 1 means disabled, 0 means enabled.
-  SpellsBitmask disabled_spells;
-  // 1 bit per secondary skill; 1 means disabled, 0 means enabled.
-  SecondarySkillsBitmask disabled_skills;
-  std::vector<Rumor> rumors;
-  HeroesSettings heroes_settings {};
-};
+  const std::map<HeroType, HeroSettings>& HeroesSettings::settings() const noexcept
+  {
+    return settings_;
+  }
 
+  struct MapAdditionalInfo
+  {
+    VictoryCondition victory_condition {};
+    LossCondition loss_condition {};
+    TeamsInfo teams;
+    // 1 bit per hero:
+    // * 0 if the hero is disabled (Map Specifications -> Heroes in the Map Editor) or already placed on the map.
+    // * 1 otherwise.
+    HeroesBitmask heroes_availability;
+    std::vector<HeroType> placeholder_heroes;
+    std::vector<CustomHero> custom_heroes;
+    // Reserved data; 0s by default.
+    ReservedData<31> reserved {};
+    // 1 bit per artifact; 1 means disabled, 0 means enabled.
+    // Spell Book, War Machines and Grail are not affected by this bitmask: they are enabled by default
+    // by the Map Editor, but you will be able to purchase them (or dig them out in case of Grail) even
+    // if you disable them manually.
+    ArtifactsBitmask disabled_artifacts;
+    // 1 bit per spell; 1 means disabled, 0 means enabled.
+    SpellsBitmask disabled_spells;
+    // 1 bit per secondary skill; 1 means disabled, 0 means enabled.
+    SecondarySkillsBitmask disabled_skills;
+    std::vector<Rumor> rumors;
+    HeroesSettings heroes_settings {};
+  };
 }
